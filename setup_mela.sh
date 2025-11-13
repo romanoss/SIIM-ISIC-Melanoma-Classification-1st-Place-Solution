57203baa23fe5981960c2c7920b2a1109cc7703b
romanoss <roman_weilguny@fire.at>
2025-11-13 12:16:59 +0100

    init runpod

---
+++ setup_mela.sh
@@ -0,0 +1,13 @@
+pip install kaggle
+cd workspace
+mkdir ./data
+cd ./data
+for input_size in 384 #512 768 1024
+do
+  kaggle datasets download -d cdeotte/jpeg-isic2019-${input_size}x${input_size}
+  kaggle datasets download -d cdeotte/jpeg-melanoma-${input_size}x${input_size}
+  unzip -q jpeg-melanoma-${input_size}x${input_size}.zip -d jpeg-melanoma-${input_size}x${input_size}
+  unzip -q jpeg-isic2019-${input_size}x${input_size}.zip -d jpeg-isic2019-${input_size}x${input_size}
+  rm jpeg-melanoma-${input_size}x${input_size}.zip jpeg-isic2019-${input_size}x${input_size}.zip
+done
+
